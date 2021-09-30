e4deae12b46660d68dd26b42e946c66320e19471
Nabeel Amjad <nabeelamjad@Mac-Nabeel-SE-053.local>
2021-09-30 10:53:13 +0500

    project setup

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: ['airbnb-base', 'eslint:recommended', 'prettier:recommended'],
+  plugins: ['prettier'],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    'no-console': 'off',
+    'max-classes-per-file': ['error', 2],
+    'global-require': 'off',
+  },
+};
---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const port = process.env.PORT;
+
+// app = app.Route();
+app.get('/', function (req, res) {
+  res.writeHead(200, { 'content-type': 'text/html' });
+  res.end(
+    '<h1 style="text-align:center">Welcome to InvoZone NodeJS Training Session</h1>',
+  );
+});
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+module.exports = app;
