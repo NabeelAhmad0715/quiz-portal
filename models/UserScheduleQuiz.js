8fb6b0aa50a1c84484e01d3c4cdd47d1d37d5154
Nabeel Amjad <nabeelamjad@Mac-Nabeel-SE-053.local>
2021-10-05 15:09:49 +0500

    change models and migrations

--- models/UserScheduleQuiz.js
+++ models/UserScheduleQuiz.js
@@ -1,26 +1,26 @@
 const { Model } = require('sequelize');
 
 module.exports = (sequelize, DataTypes) => {
-  class Admin extends Model {
+  class UserScheduleQuiz extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate() {
+    static associate(_models) {
       // define association here
     }
   }
-  Admin.init(
+  UserScheduleQuiz.init(
     {
-      name: DataTypes.STRING,
-      email: DataTypes.STRING,
-      password: DataTypes.STRING,
+      user_id: DataTypes.INTEGER,
+      schedule_quiz_id: DataTypes.INTEGER,
     },
+    { timestamps: false },
     {
       sequelize,
-      modelName: 'Admin',
+      modelName: 'UserScheduleQuiz',
     },
   );
-  return Admin;
+  return UserScheduleQuiz;
 };
